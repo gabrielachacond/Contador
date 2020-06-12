697e4c31045d242f271310e1c68460d927a05b41
gabrielachacond <54561227+gabrielachacond@users.noreply.github.com>
2020-06-11 21:10:50 -0300

    descontar-reset

--- src/App.js
+++ src/App.js
@@ -16,12 +16,23 @@ class Contador extends React.Component {
   increment = () => {
     this.setState(prevState => ({ count: prevState.count + 1 }));
   };
+
+  decrement = () => {
+    this.setState(prevState => ({ count: prevState.count - 1 }));
+  };
+
+  reset = () => {
+    this.setState(prevState => ({ count: 0 }));
+  };
+
   render() {
     return (
       <div className="App">
         <h2>¡Hola!</h2>
         <p> {this.state.count} </p>
+        <button onClick={this.decrement}>descontar contador </button>
         <button onClick={this.increment}> Incrementar contador </button>
+        <button onClick={this.reset}> Reiniciar contador </button>
       </div>
     );
   }
